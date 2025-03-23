32678250dfbb64bdcc81577db6c1b157b8934f28
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-03-23 16:24:49 +0200

    feat(sdk-patcher): make uninstall in SdkPatcherRule nullable

--- packages/sdk-patcher/src/rules/sdk-patcher-rule.ts
+++ packages/sdk-patcher/src/rules/sdk-patcher-rule.ts
@@ -2,5 +2,5 @@ import { WmeSDK } from 'wme-sdk-typings';
 
 export interface SdkPatcherRule {
   install(sdk: WmeSDK): void | Promise<void>;
-  uninstall(sdk: WmeSDK): void | Promise<void>;
+  uninstall?(sdk: WmeSDK): void | Promise<void>;
 }
