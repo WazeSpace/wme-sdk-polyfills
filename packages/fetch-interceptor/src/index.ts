b5825f6eb71020d64ed5f1ac6409f6b22a2637ea
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-03-23 16:43:39 +0200

    feat(fetch-interceptor): export should intercept loaders and create response

--- packages/fetch-interceptor/src/index.ts
+++ packages/fetch-interceptor/src/index.ts
@@ -3,3 +3,7 @@ import { FetchInterceptor } from './lib/fetch-interceptor.js';
 
 export { FetchInterceptor };
 export const defaultFetchInterceptor = new FetchInterceptor(getWindow());
+
+export * from './lib/should-intercept-loaders/index.js';
+export * from './lib/create-response.js';
+export * from './lib/create-json-response.js';
