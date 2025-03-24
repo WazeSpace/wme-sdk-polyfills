76b27595b19e527073eb15d3b947a777524128f4
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2025-03-24 17:24:08 +0200

    fix(wme-utils): resolveEntityPrototype caused ongoing getFeature requests to cancel
    Issue: The descartesClient creates a new AbortController every time we request new features, assuming the old requests is obsolete, and the new request will return relevant data. We however use this function to return a dummy element just to retrieve its type.
    
    Fix: Patch the `descartesClient.createAbortController` to return an actual `AbortController` without cancelling existing `AbortControllers`, so the original requests will be resolved as normally in parallel to our fake data returned by the native `getFeatures` function.

--- packages/wme-utils/src/lib/resolve-entity-prototype.ts
+++ packages/wme-utils/src/lib/resolve-entity-prototype.ts
@@ -1,6 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getWindow } from '@wme-enhanced-sdk/utils';
 import { defaultFetchInterceptor, isOfEndpoint, createJsonResponse } from '@wme-enhanced-sdk/fetch-interceptor';
+import { PropertySwapper } from '@wme-enhanced-sdk/method-interceptor';
+
+function patchAbortControllerOnce() {
+  const window = getWindow<{ W: any }>();
+  const swapper = new PropertySwapper(window.W.controller.descartesClient, 'createAbortController');
+  swapper.swap(() => {
+    swapper.restore();
+    return new AbortController();
+  });
+}
 
 function getFeatures<RN extends string>(repositoryName: RN): Promise<{
   [K in RN]: {
@@ -8,6 +18,9 @@ function getFeatures<RN extends string>(repositoryName: RN): Promise<{
   };
 }> {
   const window = getWindow<{ W: any }>();
+  // patch the createAbortController to return a new instance which doesn't aborts ongoing requests
+  // so they can be resolved normally
+  patchAbortControllerOnce();
   return window.W.controller.descartesClient.getFeatures(
     [0,0,0,0],
     { [repositoryName]: true },
